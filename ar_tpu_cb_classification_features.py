df22fcb53d002d237798b52e61798d5b675f985f
Mahdi <azzeddine.benaissa@ensi-uma.tn>
2022-08-20 13:14:33 +0100

    Fixed some bugs

--- ar_tpu_cb_classification_features.py
+++ ar_tpu_cb_classification_features.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep  8 16:14:36 2021
+Created on Wed Oct 27 16:08:38 2021
+
+@author: IT Doctor
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 14:03:07 2021
 
 @author: IT Doctor
 """
@@ -15,12 +21,14 @@ import os
 from tensorflow import keras
 import tensorflow_addons as tfa
 
-from transformers import BertTokenizer, RobertaTokenizer, DistilBertTokenizer
-from transformers import TFBertModel, TFRobertaModel, TFDistilBertModel
+from transformers import XLMRobertaTokenizer, AutoTokenizer
+from transformers import TFXLMRobertaModel, TFAutoModel, AutoConfig
 from transformers import logging as hf_logging
 
 from sklearn.utils import class_weight
-import load_data_module, clf_models, iffl_loss
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import load_data_module, clf_tpu_models, iffl_loss
 from results_prediction_module import print_learning_curves, predict_and_visualize
 
 #### Inputs encoding function
@@ -38,7 +46,14 @@ def hf_model_encode(data, maximum_length, tokenizer) :
       )     
       input_ids.append(encoded['input_ids'])
       attention_masks.append(encoded['attention_mask'])
-  return np.array(input_ids), np.array(attention_masks)
+  return np.array(input_ids),np.array(attention_masks)
+
+#### Features Scaling Function
+def FeaturesScaling(features):
+  minmax_scaler = MinMaxScaler()
+  minmax_scaler.fit(features)
+  scaled_features = minmax_scaler.transform(features)
+  return minmax_scaler, scaled_features.astype(dtype='float32')  
 
 # The below is necessary for starting Numpy generated random numbers
 # in a well-defined initial state.
@@ -92,145 +107,166 @@ if __name__ == '__main__':
     clf_model = args.clf_model
     cs_method = args.cost_sensitive
     
-    if nlp_model == 'bert':
-        #### Loading tokenizer
-        tokenizer = BertTokenizer.from_pretrained('bert-large-uncased', do_lower_case=False)
-        #### Loading TF BERT model
-        hf_model = TFBertModel.from_pretrained('bert-large-uncased')
-    elif nlp_model == 'roberta':
-        tokenizer  = RobertaTokenizer.from_pretrained('roberta-large', do_lower_case=False)
-        hf_model = TFRobertaModel.from_pretrained('roberta-large')     
-    elif nlp_model == 'distilbert':
-        tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased', do_lower_case=False)
-        hf_model = TFDistilBertModel.from_pretrained('distilbert-base-cased')   
-        
-    X_train, X_train_stylometric, X_train_readability, X_train_lexical, X_train_liwc, X_train_sentiments, y_train = load_data_module.load_train_features(dataset_name, eda)
-    X_valid, X_valid_stylometric, X_valid_readability, X_valid_lexical, X_valid_liwc, X_valid_sentiments, y_valid = load_data_module.load_valid_features(dataset_name, eda)
-    X_test, X_test_stylometric, X_test_readability, X_test_lexical, X_test_liwc, X_test_sentiments, y_test = load_data_module.load_test_features(dataset_name, eda)   
+    use_tpu = True #@param {type:"boolean"}
+
+    if use_tpu:
+        assert 'COLAB_TPU_ADDR' in os.environ, 'Missing TPU; did you request a TPU in Notebook Settings?'
+
+    if 'COLAB_TPU_ADDR' in os.environ:
+      TF_MASTER = 'grpc://{}'.format(os.environ['COLAB_TPU_ADDR'])
+    else:
+      TF_MASTER=''
+
+    # TPU address
+    tpu_address = TF_MASTER
+    print("############ The TPU address is {} \n".format(tpu_address))
     
-    #======================================================================
-    #================== Features Scaling ==================================
-    #======================================================================
+    ### TPU Config
+    resolver = tf.distribute.cluster_resolver.TPUClusterResolver(TF_MASTER)
+    tf.config.experimental_connect_to_cluster(resolver)
+    tf.tpu.experimental.initialize_tpu_system(resolver)
+    strategy = tf.distribute.TPUStrategy(resolver)
 
-    stylometric_scaler, X_train_stylometric =  FeaturesScaling(X_train_stylometric)
-    X_test_stylometric = stylometric_scaler.transform(X_test_stylometric)
-    X_valid_stylometric = stylometric_scaler.transform(X_valid_stylometric)
-
-    readability_scaler, X_train_readability =  FeaturesScaling(X_train_readability)
-    X_test_readability = readability_scaler.transform(X_test_readability)
-    X_valid_readability = readability_scaler.transform(X_valid_readability)
-
-    lexical_scaler, X_train_lexical =  FeaturesScaling(X_train_lexical)
-    X_test_lexical = lexical_scaler.transform(X_test_lexical)
-    X_valid_lexical = lexical_scaler.transform(X_valid_lexical)
-
-    # liwc_scaler, X_train_liwc =  FeaturesScaling(X_train_liwc)
-    # X_test_liwc = liwc_scaler.transform(X_test_liwc)
-    # X_valid_liwc = liwc_scaler.transform(X_valid_liwc)
-
-    sentiments_scaler, X_train_sentiments =  FeaturesScaling(X_train_sentiments)
-    X_test_sentiments = sentiments_scaler.transform(X_test_sentiments)
-    X_valid_sentiments = sentiments_scaler.transform(X_valid_sentiments)
-
-    #=======================================================================
-
-    MAX_LEN = 40    
-    ##### Preparing train and test data
-    X_train_ids, X_train_masks = hf_model_encode(X_train, MAX_LEN, tokenizer)
-    X_valid_ids, X_valid_masks = hf_model_encode(X_valid, MAX_LEN, tokenizer)
-    X_test_ids, X_test_masks   = hf_model_encode(X_test, MAX_LEN, tokenizer)
-    
-    if clf_model == 'mcnn_with_features':
-        model = clf_models.mcnn_model_with_features(hf_model)
-    elif clf_model =='blstm_mcnn_with_features':
-        model = clf_models.blstm_mcnn_model_with_features(hf_model)
-    elif clf_model =='mcnn_blstm_with_features':
-        model = clf_models.mcnn_blstm_model_with_features(hf_model)
-
-    model.summary()
-    
-    if eda == 'y':
-        chkp_path = './' + dataset_name + '_eda_' + nlp_model + '_' + clf_model + '.h5'
-        mchkp = keras.callbacks.ModelCheckpoint(chkp_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only = True)
-        model.compile(keras.optimizers.Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
-        history = model.fit(
-            [X_train_ids, X_train_masks, X_train, X_train_lexical, X_train_readability, X_train_sentiments, X_train_stylometric], 
-            y_train, 
-            epochs=4, 
-            batch_size=10, 
-            validation_data=([X_valid_ids, X_valid_masks, X_valid, X_valid_lexical, X_valid_readability, X_valid_sentiments, X_valid_stylometric], y_valid),
-            callbacks=[mchkp]
-            )
-        path = dataset_name + '_eda_' + nlp_model + '_' + clf_model
+    with strategy.scope():
+        tokenizer  = AutoTokenizer.from_pretrained('jplu/tf-xlm-roberta-base', do_lower_case=False)
+        config_path = '/content/drive/MyDrive/config.json'
+        # config = AutoConfig.from_pretrained(config_path)
+        config = AutoConfig.from_pretrained('xlm-roberta-base')
+        hf_model = TFAutoModel.from_pretrained('jplu/tf-xlm-roberta-base', config=config)     
         
-    elif eda == 'n' and cs_method == 'cw':
-        chkp_path = './' + dataset_name + '_' + nlp_model + '_' + clf_model + '_cw.h5'
-        mchkp = keras.callbacks.ModelCheckpoint(chkp_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only = True)
-        model.compile(keras.optimizers.Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
-        cw = class_weight.compute_class_weight(class_weight = 'balanced',  classes = [0, 1], y = y_train)
-        cw_dict = {0: cw[0], 1: cw[1]}
+        ##### Load data
+        X_train, X_test = load_data_module.load_data(dataset_name)
+        y_train, y_test = load_data_module.load_labels(dataset_name)
+        X_train_use, X_test_use = load_data_module.load_use_embeddings(dataset_name)
         
-        history = model.fit(
-            [X_train_ids, X_train_masks, X_train, X_train_lexical, X_train_readability, X_train_sentiments, X_train_stylometric],
-            y_train, 
-            epochs=4, 
-            batch_size=10, 
-            validation_data=([X_valid_ids, X_valid_masks, X_valid], y_valid),
-            callbacks=[mchkp],
-            class_weight=cw_dict
-            )
-        path = dataset_name + '_' + nlp_model + '_' + clf_model + '_cw'
+        X_train_use, X_test_use = np.asarray(X_train_use), np.asarray(X_test_use)
         
-    elif eda == 'n' and cs_method == 'focal':
-        chkp_path = './' + dataset_name + '_' + nlp_model + '_' + clf_model + '_focal_.h5'
-        mchkp = keras.callbacks.ModelCheckpoint(chkp_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only = True)
-        model.compile(keras.optimizers.Adam(lr=6e-6), loss=tfa.losses.SigmoidFocalCrossEntropy(alpha=0.925, gamma=0.99), metrics=['accuracy'])
-        history = model.fit(
-            [X_train_ids, X_train_masks, X_train, X_train_lexical, X_train_readability, X_train_sentiments, X_train_stylometric],
-            y_train, 
-            epochs=4, 
-            batch_size=10, 
-            validation_data=([X_valid_ids, X_valid_masks, X_valid, X_valid_lexical, X_valid_readability, X_valid_sentiments, X_valid_stylometric], y_valid),
-            callbacks=[mchkp]
-            )        
-        path = dataset_name + '_' + nlp_model + '_' + clf_model + '_focal'
+        MAX_LEN = 40    
         
-    elif eda == 'n' and cs_method == 'iffl':
-        chkp_path = './' + dataset_name + '_' + nlp_model + '_' + clf_model + '_iffl.h5'
-        cw = class_weight.compute_class_weight('balanced',  [0, 1], y_train)
-        cw_dict = {0: cw[0], 1: cw[1]}
-        mchkp = keras.callbacks.ModelCheckpoint(chkp_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only = True)
-        model.compile(keras.optimizers.Adam(lr=6e-6), loss= iffl_loss.if_focal_loss(alpha=0.65, gamma=0.55, class_weights=cw_dict), metrics=['accuracy'])
-         
-        history = model.fit(
-            [X_train_ids, X_train_masks, X_train],
-            y_train, 
-            epochs=4, 
-            batch_size=10, 
-            validation_data=([X_valid_ids, X_valid_masks, X_valid], y_valid),
-            callbacks=[mchkp]
-            )        
-        path = dataset_name + '_' + nlp_model + '_' + clf_model + '_iffl'        
-    
-    
-# =============================================================================
-#     Create a new directory with the model and dataset names
-#         - Contains the learning curves figures
-#         - The clssification report as a csv file 
-#         - The confusion matrix figure
-#         - The trained model
-#     
-# =============================================================================
+        ##### Split data
+        X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.2, random_state = 42)
+        X_train_use, X_valid_use = train_test_split(X_train_use, test_size = 0.2, random_state = 42)
+        
+        ##### Preparing train and test data
+        X_train_ids, X_train_masks = hf_model_encode(X_train, MAX_LEN, tokenizer)
+        X_valid_ids, X_valid_masks = hf_model_encode(X_valid, MAX_LEN, tokenizer)
+        X_test_ids, X_test_masks   = hf_model_encode(X_test, MAX_LEN, tokenizer)
+
+        #================= Loading Features ===================================
+        _, X_train_stylometric, X_train_readability, X_train_lexical, X_train_liwc, X_train_sentiments, y_train = load_data_module.load_train_features(dataset_name, eda)
+        _, X_valid_stylometric, X_valid_readability, X_valid_lexical, X_valid_liwc, X_valid_sentiments, y_valid = load_data_module.load_valid_features(dataset_name, eda)
+        _, X_test_stylometric, X_test_readability, X_test_lexical, X_test_liwc, X_test_sentiments, y_test = load_data_module.load_test_features(dataset_name, eda)   
+        
+        #======================================================================
+        #================== Features Scaling ==================================
+        #======================================================================
+
+        stylometric_scaler, X_train_stylometric =  FeaturesScaling(X_train_stylometric)
+        X_test_stylometric = stylometric_scaler.transform(X_test_stylometric)
+        X_valid_stylometric = stylometric_scaler.transform(X_valid_stylometric)
+
+        readability_scaler, X_train_readability =  FeaturesScaling(X_train_readability)
+        X_test_readability = readability_scaler.transform(X_test_readability)
+        X_valid_readability = readability_scaler.transform(X_valid_readability)
+
+        lexical_scaler, X_train_lexical =  FeaturesScaling(X_train_lexical)
+        X_test_lexical = lexical_scaler.transform(X_test_lexical)
+        X_valid_lexical = lexical_scaler.transform(X_valid_lexical)
+
+        # liwc_scaler, X_train_liwc =  FeaturesScaling(X_train_liwc)
+        # X_test_liwc = liwc_scaler.transform(X_test_liwc)
+        # X_valid_liwc = liwc_scaler.transform(X_valid_liwc)
+
+        sentiments_scaler, X_train_sentiments =  FeaturesScaling(X_train_sentiments)
+        X_test_sentiments = sentiments_scaler.transform(X_test_sentiments)
+        X_valid_sentiments = sentiments_scaler.transform(X_valid_sentiments)
+
+        #=======================================================================
+
      
-    os.mkdir(path)
-    print_learning_curves(history, path)
-    model.load_weights(chkp_path)
-    clf_report, confusion_matrix_fig = predict_and_visualize(model, [X_test_ids, X_test_masks, X_test], y_test)
-    
-    ### Saving the classification report as a CSV file
-    clf_report_df = pd.DataFrame(clf_report).transpose()
-    clf_report_df.to_csv(path + '/classification_report.csv') 
-    ### Saving the confusion matrix figure
-    confusion_matrix_fig.savefig(path + '/confusion_matrix')
-    ### Saving the model
-    model.save_weights(path + '/saved_weights')
+        if clf_model == 'mcnn_with_features':
+            model = clf_tpu_models.mcnn_model_tpu_with_features(hf_model)
+        elif clf_model =='blstm_mcnn_with_features':
+            model = clf_tpu_models.blstm_mcnn_model_tpu_with_features(hf_model)
+        elif clf_model =='mcnn_blstm_with_features':
+            model = clf_tpu_models.mcnn_blstm_model_tpu_with_features(hf_model)
+
+        model.compile(keras.optimizers.Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
+        model.summary()
+        
+        if eda == 'y':
+            chkp_path = './' + dataset_name + '_eda_' + nlp_model + '_' + clf_model + '.h5'
+            mchkp = keras.callbacks.ModelCheckpoint(chkp_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only = True)
+            model.compile(keras.optimizers.Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
+            history = model.fit(
+            [X_train_ids, X_train_masks, X_train, X_train_lexical, X_train_readability, X_train_sentiments, X_train_stylometric], 
+                y_train, 
+                epochs=4, 
+                batch_size=10, 
+            validation_data=([X_valid_ids, X_valid_masks, X_valid, X_valid_lexical, X_valid_readability, X_valid_sentiments, X_valid_stylometric], y_valid),
+                callbacks=[mchkp]
+                )
+            path = dataset_name + '_eda_' + nlp_model + '_' + clf_model
+            
+        elif eda == 'n' and cs_method == 'cw':
+            chkp_path = './' + dataset_name + '_' + nlp_model + '_' + clf_model + '_cw.h5'
+            mchkp = keras.callbacks.ModelCheckpoint(chkp_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only = True)
+            # model.compile(keras.optimizers.Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
+            cw = class_weight.compute_class_weight(class_weight = 'balanced',  classes = [0, 1], y = y_train)
+            cw_dict = {0: cw[0], 1: cw[1]}
+            
+            history = model.fit(
+                [X_train_ids, X_train_masks, X_train, X_train_lexical, X_train_readability, X_train_sentiments, X_train_stylometric],
+                y_train, 
+                epochs=4, 
+                batch_size=10, 
+                validation_data=([X_valid_ids, X_valid_masks, X_valid, X_valid_lexical, X_valid_readability, X_valid_sentiments, X_valid_stylometric], y_valid),
+                callbacks=[mchkp],
+                class_weight=cw_dict
+                )
+            path = dataset_name + '_' + nlp_model + '_' + clf_model + '_cw'
+            
+        elif eda == 'n' and cs_method == 'focal':
+            chkp_path = './' + dataset_name + '_' + nlp_model + '_' + clf_model + '_focal_.h5'
+            mchkp = keras.callbacks.ModelCheckpoint(chkp_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only = True)
+            model.compile(keras.optimizers.Adam(lr=6e-6), loss=tfa.losses.SigmoidFocalCrossEntropy(alpha=0.925, gamma=0.99), metrics=['accuracy'])
+            history = model.fit(
+                [X_train_ids, X_train_masks, X_train, X_train_lexical, X_train_readability, X_train_sentiments, X_train_stylometric],
+                y_train, 
+                epochs=4, 
+                batch_size=10, 
+                validation_data=([X_valid_ids, X_valid_masks, X_valid, X_valid_lexical, X_valid_readability, X_valid_sentiments, X_valid_stylometric], y_valid),
+                callbacks=[mchkp]
+                )        
+            path = dataset_name + '_' + nlp_model + '_' + clf_model + '_focal'
+            
+        elif eda == 'n' and cs_method == 'iffl':
+            chkp_path = './' + dataset_name + '_' + nlp_model + '_' + clf_model + '_iffl.h5'
+            cw = class_weight.compute_class_weight('balanced',  [0, 1], y_train)
+            cw_dict = {0: cw[0], 1: cw[1]}
+            mchkp = keras.callbacks.ModelCheckpoint(chkp_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only = True)
+            model.compile(keras.optimizers.Adam(lr=6e-6), loss= iffl_loss.if_focal_loss(alpha=0.65, gamma=0.55, class_weights=cw_dict), metrics=['accuracy'])
+            
+            history = model.fit(
+                [X_train_ids, X_train_masks, X_train_use],
+                y_train, 
+                epochs=4, 
+                batch_size=10, 
+                validation_data=([X_valid_ids, X_valid_masks, X_valid_use], y_valid),
+                callbacks=[mchkp]
+                )        
+            path = dataset_name + '_' + nlp_model + '_' + clf_model + '_iffl'        
+        
+        
+        os.mkdir(path)
+        print_learning_curves(history, path)
+        model.load_weights(chkp_path)
+        clf_report, confusion_matrix_fig = predict_and_visualize(model, [X_test_ids, X_test_masks, X_test_use], y_test)
+        
+        ### Saving the classification report as a CSV file
+        clf_report_df = pd.DataFrame(clf_report).transpose()
+        clf_report_df.to_csv(path + '/classification_report.csv') 
+        ### Saving the confusion matrix figure
+        confusion_matrix_fig.savefig(path + '/confusion_matrix')
+        ### Saving the model
+        model.save_weights(path + '/saved_weights')
--- clf_tpu_models.py
+++ clf_tpu_models.py
@@ -116,3 +116,126 @@ def mcnn_blstm_model_tpu(bert_model):
   # model.compile(keras.optimizers.Adam(lr=6e-6), loss=tfa.losses.SigmoidFocalCrossEntropy(alpha=0.925, gamma=0.99), metrics=['accuracy'])
   # model.compile(keras.optimizers.Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
   return model
+
+####################################################
+######      Models with input features        ######
+####################################################
+
+def mcnn_model_tpu_with_features(bert_model):
+  np.random.seed(123)
+  python_random.seed(123)
+  tf.random.set_seed(1234)
+
+  input_ids          = keras.layers.Input(shape=(40,),dtype='int32')
+  attention_masks    = keras.layers.Input(shape=(40,),dtype='int32')
+  use_inputs         = keras.layers.Input(shape=(512,),dtype='float32')
+
+  input_lexical     = keras.layers.Input(shape=(17,),dtype='float32')
+  input_readability = keras.layers.Input(shape=(16,),dtype='float32')
+  input_sentiments  = keras.layers.Input(shape=(6,),dtype='float32')
+  input_stylometric = keras.layers.Input(shape=(4,),dtype='float32')
+  
+  output = bert_model([input_ids,attention_masks])
+  output = output[0]
+  # bilstm = keras.layers.Bidirectional(keras.layers.LSTM(40, activation='relu', return_sequences=True))(output)
+  conv1 = keras.layers.Conv1D(32, 2, activation='relu')(output)
+  conv2 = keras.layers.Conv1D(32, 4, activation='relu')(output)
+  conv3 = keras.layers.Conv1D(32, 6, activation='relu')(output)
+
+  pool1 = keras.layers.GlobalMaxPooling1D()(conv1)
+  pool2 = keras.layers.GlobalMaxPooling1D()(conv2)
+  pool3 = keras.layers.GlobalMaxPooling1D()(conv3)
+
+
+  concat = keras.layers.Concatenate()([pool1, pool2, pool3, use_inputs, input_lexical, input_readability, input_sentiments, input_stylometric])
+
+  dense = keras.layers.Dense(64,activation='relu')(concat)
+  dense = keras.layers.Dropout(0.2)(dense)
+  output = keras.layers.Dense(1, activation='sigmoid')(dense)
+
+  # loss_ = SigmoidFocalCrossEntropy_1(alpha=0.65, gamma=0.55)
+
+  model = keras.models.Model(inputs = [input_ids, attention_masks, use_inputs, input_lexical, input_readability, input_sentiments, input_stylometric],outputs = output)
+  # model.compile(keras.optimizers.Adam(lr=6e-6), loss=tfa.losses.SigmoidFocalCrossEntropy(alpha=0.925, gamma=0.99), metrics=['accuracy'])
+  # model.compile(keras.optimizers.Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
+  return model
+
+def blstm_mcnn_model_tpu_with_features(bert_model):
+  np.random.seed(123)
+  python_random.seed(123)
+  tf.random.set_seed(1234)
+
+  input_ids          = keras.layers.Input(shape=(40,),dtype='int32')
+  attention_masks    = keras.layers.Input(shape=(40,),dtype='int32')
+  use_inputs         = keras.layers.Input(shape=(512,),dtype='float32')
+
+  input_lexical     = keras.layers.Input(shape=(17,),dtype='float32')
+  input_readability = keras.layers.Input(shape=(16,),dtype='float32')
+  input_sentiments  = keras.layers.Input(shape=(6,),dtype='float32')
+  input_stylometric = keras.layers.Input(shape=(4,),dtype='float32')
+
+  output = bert_model([input_ids,attention_masks])
+  output = output[0]
+  bilstm = keras.layers.Bidirectional(keras.layers.LSTM(40, activation='relu', return_sequences=True))(output)
+  conv1 = keras.layers.Conv1D(32, 2, activation='relu')(bilstm)
+  conv2 = keras.layers.Conv1D(32, 4, activation='relu')(bilstm)
+  conv3 = keras.layers.Conv1D(32, 6, activation='relu')(bilstm)
+
+  pool1 = keras.layers.GlobalMaxPooling1D()(conv1)
+  pool2 = keras.layers.GlobalMaxPooling1D()(conv2)
+  pool3 = keras.layers.GlobalMaxPooling1D()(conv3)
+
+  concat = keras.layers.Concatenate()([pool1, pool2, pool3, use_inputs, input_lexical, input_readability, input_sentiments, input_stylometric])
+
+  dense = keras.layers.Dense(64,activation='relu')(concat)
+  dense = keras.layers.Dropout(0.2)(dense)
+  output = keras.layers.Dense(1, activation='sigmoid')(dense)
+
+  # loss_ = SigmoidFocalCrossEntropy_1(alpha=0.65, gamma=0.55)
+
+  model = keras.models.Model(inputs = [input_ids, attention_masks, use_inputs, input_lexical, input_readability, input_sentiments, input_stylometric], outputs = output)
+  # model.compile(keras.optimizers.Adam(lr=6e-6), loss=tfa.losses.SigmoidFocalCrossEntropy(alpha=0.925, gamma=0.99), metrics=['accuracy'])
+  # model.compile(keras.optimizers.Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
+  return model
+
+
+def mcnn_blstm_model_tpu_with_features(bert_model):
+  np.random.seed(123)
+  python_random.seed(123)
+  tf.random.set_seed(1234)
+
+  input_ids          = keras.layers.Input(shape=(40,),dtype='int32')
+  attention_masks    = keras.layers.Input(shape=(40,),dtype='int32')
+  use_inputs         = keras.layers.Input(shape=(512,),dtype='float32')
+  
+  input_lexical     = keras.layers.Input(shape=(17,),dtype='float32')
+  input_readability = keras.layers.Input(shape=(16,),dtype='float32')
+  input_sentiments  = keras.layers.Input(shape=(6,),dtype='float32')
+  input_stylometric = keras.layers.Input(shape=(4,),dtype='float32')
+
+  output = bert_model([input_ids,attention_masks])
+  output = output[0]
+  
+  conv1 = keras.layers.Conv1D(32, 2, activation='relu', padding='same')(output)
+  conv2 = keras.layers.Conv1D(32, 4, activation='relu', padding='same')(output)
+  conv3 = keras.layers.Conv1D(32, 6, activation='relu', padding='same')(output)
+
+  pool1 = keras.layers.MaxPooling1D()(conv1)
+  pool2 = keras.layers.MaxPooling1D()(conv2)
+  pool3 = keras.layers.MaxPooling1D()(conv3)
+  
+  pool_concat = keras.layers.Concatenate()([pool1, pool2, pool3])
+  
+  bilstm = keras.layers.Bidirectional(keras.layers.LSTM(40, activation='relu'))(pool_concat)
+  concat = keras.layers.Concatenate()([bilstm, use_inputs, input_lexical, input_readability, input_sentiments, input_stylometric])
+
+  dense = keras.layers.Dense(64,activation='relu')(concat)
+  dense = keras.layers.Dropout(0.2)(dense)
+  output = keras.layers.Dense(1, activation='sigmoid')(dense)
+
+  # loss_ = SigmoidFocalCrossEntropy_1(alpha=0.65, gamma=0.55)
+
+  model = keras.models.Model(inputs = [input_ids, attention_masks, use_inputs, input_lexical, input_readability, input_sentiments, input_stylometric], outputs = output)
+  # model.compile(keras.optimizers.Adam(lr=6e-6), loss=tfa.losses.SigmoidFocalCrossEntropy(alpha=0.925, gamma=0.99), metrics=['accuracy'])
+  # model.compile(keras.optimizers.Adam(lr=6e-6), loss='binary_crossentropy', metrics=['accuracy'])
+  return model
